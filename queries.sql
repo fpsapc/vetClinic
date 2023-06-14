eb12074197dac090dcedef2c600bcabcf44c03cb
Daniel Falcon <danfall963@gmail.com>
2023-06-14 11:03:48 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
-
+SELECT * FROM animals;
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
